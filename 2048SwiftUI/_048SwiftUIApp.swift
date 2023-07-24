c464d8e2e55abd43aaaf1022b2d0a5d719f5578a
Max Valek <valek.maxwell@gmail.com>
2023-07-24 16:41:42 -0500

    Initial Commit

---
+++ 2048SwiftUI/_048SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048SwiftUIApp.swift
+//  2048SwiftUI
+//
+//  Created by Max Valek on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct _048SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
